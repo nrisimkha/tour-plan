dd949bc14d1aaacf4c0a7048fd741c57bfd3a835
nrisimkha <nrisimkha@gmail.com>
2022-05-31 16:42:05 +0300

    create: booking & map

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@ const swiper = new Swiper(".swiper", {
 
 	// Navigation arrows
 	navigation: {
-		nextEl: ".swiper-button--next",
-		prevEl: ".swiper-button--prev",
+		nextEl: ".slider-button--next",
+		prevEl: ".slider-button--prev",
 	},
 });
